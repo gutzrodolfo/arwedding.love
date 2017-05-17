84cfa14837be5dd7f48843b96a3efe5164c965aa
Rodolfo Gutierrez <rodolf.gutierrez@gmail.com>
2017-05-16 21:10:36 -0700

    updated map

--- js/grayscale.js
+++ js/grayscale.js
@@ -47,7 +47,7 @@ function init() {
         zoom: 15,
 
         // The latitude and longitude to center the map (always required)
-        center: new google.maps.LatLng(21.4119, 103.1176) // Juchipila, Zac.
+        center: new google.maps.LatLng(21.4119, 103.1176), // Juchipila, Zac.
 
         // Disables the default Google Maps UI components
         disableDefaultUI: true,
