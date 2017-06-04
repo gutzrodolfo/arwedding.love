0ecc24dbdf90c53f8a20b3556187a0532a3ee50a
Rodolfo Gutierrez <Gutierrez-R2@iehp.org>
2017-06-04 00:49:30 -0700

    -

--- js/grayscale.js
+++ js/grayscale.js
@@ -17,6 +17,7 @@ $(window).scroll(function() {
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+        console.log("anchor: ", $anchor);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
