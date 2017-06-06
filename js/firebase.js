08b03b0f6aa6d329ca0fc52e24d729a194cdd664
Rodolfo Gutierrez <rodolf.gutierrez@gmail.com>
2017-06-05 19:42:53 -0700

    -

--- js/firebase.js
+++ js/firebase.js
@@ -1,12 +1,15 @@
+
+  // Initialize Firebase
+  var config = {
+    apiKey: "AIzaSyC2HltkRxPcyuOmoRR3doDv39JKXDp8-1E",
+    authDomain: "arwedding-31ea4.firebaseapp.com",
+    databaseURL: "https://arwedding-31ea4.firebaseio.com",
+    projectId: "arwedding-31ea4",
+    storageBucket: "arwedding-31ea4.appspot.com",
+    messagingSenderId: "1035994472977"
+  };
+  firebase.initializeApp(config);
+
 // Get a reference to the database service
 var database = firebase.database();
-
-function writeUserData(userId, name, email, imageUrl) {
-  firebase.database().ref('users/' + userId).set({
-    username: name,
-    email: email,
-    profile_picture : imageUrl
-  });
-}
-
-  console.log("database: ", databse.ref());
+console.log("database: ", databse.ref());
