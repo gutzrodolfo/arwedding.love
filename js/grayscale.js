b3f733120ed1cb5df1480a9d406c7280c8adfcb2
Rodolfo Gutierrez <rodolf.gutierrez@gmail.com>
2017-06-07 21:57:40 -0700

    -

--- js/grayscale.js
+++ js/grayscale.js
@@ -45,7 +45,7 @@ function init() {
   // For more options see: https://developers.google.com/maps/documentation/javascript/reference#MapOptions
   var mapOptions = {
     // How zoomed in you want the map to start at (always required)
-    zoom: 13,
+    zoom: 16,
 
     // The latitude and longitude to center the map (always required)
     center: new google.maps.LatLng(21.411912, -103.11755), // Juchipila, Zac.
